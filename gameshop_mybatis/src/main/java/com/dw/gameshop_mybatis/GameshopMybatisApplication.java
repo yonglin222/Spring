309280d033704ea70ff4b29d482393a0cb59bd6d
yonglin222 <dydfls0429@gmail.com>
2025-08-04 10:16:29 +0900

    Add files via upload

---
+++ gameshop_mybatis/src/main/java/com/dw/gameshop_mybatis/GameshopMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.dw.gameshop_mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameshopMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameshopMybatisApplication.class, args);
+	}
+
+}
